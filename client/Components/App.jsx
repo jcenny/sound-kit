897db5a1a7dcc5a7d2b6e5129cf2b07a87d57dff
jcenny <jcenny26@gmail.com>
2020-12-05 20:50:11 -0800

    Refactored to React Hooks

--- client/Components/App.jsx
+++ client/Components/App.jsx
@@ -1,48 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { sampleSounds } from '../sampleSounds.js'
 
-class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      keys: sampleSounds,
+const App = () => {
+  const [keys, setKeys] = useState([]);
+  
+  useEffect(() => {
+    setKeys(sampleSounds);
+
+    const handleKeyPress = (e) => {
+      const audio = document.querySelector(`audio[data-key="${e.key}"]`);
+      if (!audio) return;
+      audio.currentTime = 0;
+      audio.play();
     }
-    this.handleKeyPress = this.handleKeyPress.bind(this);
-  }
 
-  componentDidMount() {
-    document.addEventListener('keydown', this.handleKeyPress);
-  }
+    document.addEventListener('keydown', handleKeyPress)
 
-  componentWillUnmount() {
-    document.removeEventListener('keydown', this.handleKeyPress);
-  }
+    return function cleanUp () {
+      document.removeEventListener('keydown', handleKeyPress)
+    }
+  })
 
-  handleKeyPress(e) {
-    const audio = document.querySelector(`audio[data-key="${e.key}"]`);
-    if (!audio) return;
-    audio.currentTime = 0;
-    audio.play();
-  }
-
-  render() {
-    const { keys } = this.state;
-    return (
-      <div>
-        {keys.map((set) => {
-          let { key, sound } = set;
-          return (
-            <div>
-              <div data-key={key}>
-                <kbd>{key.toUpperCase()}</kbd>
-              </div>
-              <audio data-key={key} src={`./sounds/${sound}`}></audio>
+  return (
+    <div>
+      {keys.map((set) => {
+        let { key, sound } = set;
+        return (
+          <div>
+            <div data-key={key}>
+              <kbd>{key.toUpperCase()}</kbd>
+              <span> {sound.substring(0, sound.length - 4)}</span>
             </div>
-          )
-        })}
-      </div>
-    )
-  }
+            <audio data-key={key} src={`./sounds/${sound}`}></audio>
+          </div>
+        )
+      })}
+    </div>
+  )
 }
 
 export default App;
